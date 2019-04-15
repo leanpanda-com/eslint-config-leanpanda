efd144726ba4891bb702edee405a74752ee437e6
Joe Yates <joe.g.yates@gmail.com>
2019-04-15 17:48:17 +0200

    Avoid errors in immer produce

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ module.exports = {
     'comma-dangle': ['error', 'only-multiline'],
     'max-len': ['error', {'code': 80}],
     'no-multiple-empty-lines': ['error', {max: 1, maxEOF: 0}],
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['draft', 'draftState']
+      }
+    ],
     'no-unused-vars': ['error', {'argsIgnorePattern': '^_'}],
     'object-curly-newline': ['error', {
       'ObjectExpression': {'multiline': true},
